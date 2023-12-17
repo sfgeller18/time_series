ed6d934d857ca45312400e9a22ba0018eb509342
sfgeller18 <sfgeller18@gmail.com>
2023-12-17 12:14:55 -0800

    commit

--- include/CsvManager.hpp
+++ include/CsvManager.hpp
@@ -11,12 +11,7 @@ template <typename T>
 void WriteToCSV(const std::string& filename, const Vector<T>& x)
 {
    std::ofstream myfile(filename);
-
-   #ifdef USE_ARMA
-   int nrows = x.n_rows;
-   #elif defined(USE_BLAZE) || defined(USE_EIGEN)
    int nrows = x.size();
-   #endif
 
    for (int i = 0; i < nrows; ++i)
       myfile << x[i] << "\n";
@@ -104,35 +99,17 @@ void ReadFromCSV(const std::string& filename, Matrix<T>& x)
 
       std::stringstream lineStream(line);
       std::string cell;
-
-      #ifdef USE_ARMA
-      arma::Row<T> z;
-      #elif USE_BLAZE
-      blaze::DynamicVector<T,blaze::rowVector> z;
-      #elif USE_EIGEN
       Vector<T> z;
-      #endif
 
       int ncols = 0;
 
       while (std::getline(lineStream, cell, ',')) {
-         #if defined(USE_ARMA) || defined(USE_BLAZE)
-         z.resize(ncols + 1);
-         #elif USE_EIGEN
          z.conservativeResize(ncols + 1, Eigen::NoChange);
-         #endif
          z[ncols] = std::stod(cell);
          ++ncols;
       }
-      #ifdef USE_ARMA
-      x.insert_rows(nrows, z);
-      #elif USE_BLAZE
-      x.resize(nrows + 1, ncols);
-      row(x, nrows) = z;
-      #elif USE_EIGEN
       x.conservativeResize(nrows + 1, ncols);
       x.row(nrows) = z;
-      #endif
       ++nrows;
    }
    myfile.close();
